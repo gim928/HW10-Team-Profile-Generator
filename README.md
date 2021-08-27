9d6e5c157decb7a9d27b9ec2d7b6561ba94d599d
Gina Im <eaim928@gmail.com>
2021-08-27 08:28:02 -0600

    added readme and video link

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# HW8-Team-Profile-Generator
+# Team Profile Generator
+
+## License
+
+    This project is licensed with MIT.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+This is a command line application that helps the user create a profile page for their team. The application generates an HTML file dynamically based on user input that displays each team member's information on each card. Each team member type displays different information specific to each job type (manager, intern, and engineer).
+
+## Table of Contents
+
+1. [License](#license)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Tests](#tests)
+5. [Contributing](#contributing)
+6. [Questions](#questions)
+
+## Installation
+
+npm i
+
+## Usage
+
+This project will be used to generate team profile pages easily.
+
+## Tests
+
+npm run test
+
+## Contributing
+
+Run node index.js and answer the prompts in the terminal. When you are done adding team members the HTML file will be generated.
+
+## Questions
+
+Questions?
+
+You may reach me by email at: <eaim928@gmail.com>
+
+You may also reach me on github: <https://github.com/gim928>
+
+Live link to the application: https://gim928.github.io/HW8-Team-Profile-Generator/
+
+Video DEMO: https://drive.google.com/file/d/1sDy8nqf9Y5eJ_6_mSf7OLnZYyyZXMLKp/view
--- src/generateTeam.js
+++ src/generateTeam.js
@@ -1,10 +1,12 @@
+//requiring in fs
 const fs = require("fs");
-const Employee = require("../types/employee");
-const Manager = require("../types/manager");
-const Intern = require("../types/intern");
-const Engineer = require("../types/engineer");
-const index = require("../index");
+// const Employee = require("../types/employee");
+// const Manager = require("../types/manager");
+// const Intern = require("../types/intern");
+// const Engineer = require("../types/engineer");
+// const index = require("../index");
 
+//creates the HTML document with the employee cards inserted
 const generateHTML = function (employeeJoin) {
   return `
     <!DOCTYPE html>
@@ -14,13 +16,12 @@ const generateHTML = function (employeeJoin) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Team Profile</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
   </head>
   <body>
       <header>
           <nav class="navbar" id="navbar">
-              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+              <span class="navbar-nav mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
           </nav>
       </header>
       <main>
@@ -31,40 +32,45 @@ const generateHTML = function (employeeJoin) {
               </div>
           </div>
       </main>
-      
-  </body>
+       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+ 
   </html>
 `;
 };
 
+//creates a card for the manager
 const generateManager = function (Manager) {
   return `
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
+<div class="card m-4 shadow-lg p-3 mb-5 bg-light rounded" style="width: 18rem;">
+  <div class="card-body custom-header">
     <h3 class="card-title">Manager</h3>
     <h4>${Manager.name}</h4>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${Manager.id}</li>
-    <li class="list-group-item">Email: ${Manager.email}</li>
-    <li class="list-group-item">Office Number: ${Manager.officeNum}</li>
+    <li class="list-group-item"id="custom-list">ID: ${Manager.id}</li>
+    <li class="list-group-item"id="custom-list">Email: ${Manager.email}</li>
+    <li class="list-group-item"id="custom-list">Office Number: ${Manager.officeNum}</li>
   </ul>
 </div>
 `;
 };
 
+//creates a card for an engineer
 const generateEngineer = function (Engineer) {
   return `
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
+<div class="card m-4 shadow-lg p-3 mb-5 bg-light rounded" style="width: 18rem;">
+  <div class="card-body custom-header">
     <h3 class="card-title">Engineer</h3>
     <h4>${Engineer.name}</h4>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${Engineer.id}</li>
-    <li class="list-group-item">Email: ${Engineer.email}</li>
-    <li class="list-group-item">Github: ${Engineer.github}</li>
+    <li class="list-group-item"id="custom-list">ID: ${Engineer.id}</li>
+    <li class="list-group-item"id="custom-list">Email: ${Engineer.email}</li>
+    <li class="list-group-item"id="custom-list">Github: ${Engineer.github}</li>
   </ul>
 </div>
 `;
@@ -72,23 +78,19 @@ const generateEngineer = function (Engineer) {
 
 const generateIntern = function (Intern) {
   return `
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
+<div class="card m-4 shadow-lg p-3 mb-5 bg-light rounded" style="width: 18rem;">
+  <div class="card-body custom-header">
     <h3 class="card-title">Intern</h3>
     <h4>${Intern.name}</h4>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${Intern.id}</li>
-    <li class="list-group-item">Email: ${Intern.email}</li>
-    <li class="list-group-item">School: ${Intern.school}</li>
+    <li class="list-group-item"id="custom-list">ID: ${Intern.id}</li>
+    <li class="list-group-item"id="custom-list">Email: ${Intern.email}</li>
+    <li class="list-group-item"id="custom-list">School: ${Intern.school}</li>
   </ul>
 </div>
 `;
 };
-
-// var employeeCards = [];
-// const employeeJoin = employeeCards.join("");
-// console.log(employeeJoin);
 
 function generateTeam(teamMembers) {
   // console.log(teamMembers);
@@ -109,10 +111,10 @@ function generateTeam(teamMembers) {
   }
   console.log(employeeCards);
   var employeeJoin = employeeCards.join("");
-  console.log(employeeJoin);
+  // console.log(employeeJoin);
   // generateHTML(employeeJoin);
   const pageHTML = generateHTML(employeeJoin);
-  console.log(pageHTML);
+  // console.log(pageHTML);
   writeToFile(pageHTML);
 }
 
